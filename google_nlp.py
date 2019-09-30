98eb6234a0f1c3e03fdd700fef653ce18a1263dd
xxLin97 <55032729+xxLin97@users.noreply.github.com>
2019-09-30 09:45:16 +0800

    Update google_nlp.py

--- google_nlp.py
+++ google_nlp.py
@@ -1,29 +1,12 @@
-# Copyright 2016, Google, Inc.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START language_sentiment_tutorial]
-"""Demonstrates how to make a simple call to the Natural Language API."""
-
-# [START language_sentiment_tutorial_imports]
 import argparse
 
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-# [END language_sentiment_tutorial_imports]
 
 
-# [START language_sentiment_tutorial_print_result]
+
+
 def print_result(annotations):
     score = annotations.document_sentiment.score
     magnitude = annotations.document_sentiment.magnitude
@@ -36,17 +19,17 @@ def print_result(annotations):
     print('Overall Sentiment: score of {} with magnitude of {}'.format(
         score, magnitude))
     return 0
-# [END language_sentiment_tutorial_print_result]
 
 
-# [START language_sentiment_tutorial_analyze_sentiment]
+
+
 def analyze(movie_review_filename):
     """Run a sentiment analysis request on text within a passed filename."""
-    client = language.LanguageServiceClient()
+    client = language.LanguageServiceClient.from_service_account_json("/media/sf_shared_folder/creds.json")
 
-    with open(movie_review_filename, 'r') as review_file:
-        # Instantiates a plain text document.
-        content = review_file.read()
+    with open("path to tweet_file") as tweet_file:
+
+        content = tweet_file.read()
 
     document = types.Document(
         content=content,
@@ -55,19 +38,9 @@ def analyze(movie_review_filename):
 
     # Print the results
     print_result(annotations)
-# [END language_sentiment_tutorial_analyze_sentiment]
 
 
-# [START language_sentiment_tutorial_run_application]
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument(
-        'movie_review_filename',
-        help='The filename of the movie review you\'d like to analyze.')
-    args = parser.parse_args()
-
-    analyze(args.movie_review_filename)
-# [END language_sentiment_tutorial_run_application]
-# [END language_sentiment_tutorial]
+          analyze("path to tweet_file")
